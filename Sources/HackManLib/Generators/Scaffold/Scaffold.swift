2d8bf1c353db5ab9edc7a027f3d84c34824e9419
Devran "Cosmo" Uenal <maccosmo@gmail.com>
2019-08-15 20:56:51 +0200

    Add scaffold support for tableViews

--- Sources/HackManLib/Generators/Scaffold/Scaffold.swift
+++ Sources/HackManLib/Generators/Scaffold/Scaffold.swift
@@ -10,9 +10,16 @@ class Scaffold: NSObject, Generator {
         ReusableView().generate(arguments: arguments, options: options)
         Coordinator().generate(arguments: arguments, options: options)
         Model().generate(arguments: arguments, options: options)
-        ViewControllerCollection().generate(arguments: arguments, options: options)
         ViewControllerDetail().generate(arguments: arguments, options: options)
-        CollectionViewCell().generate(arguments: arguments, options: options)
+
+        if options.contains("--view=table") {
+            ViewControllerTable().generate(arguments: arguments, options: options)
+            TableViewCell().generate(arguments: arguments, options: options)
+        } else {
+            ViewControllerCollection().generate(arguments: arguments, options: options)
+            CollectionViewCell().generate(arguments: arguments, options: options)
+        }
+        
         CoordinatorMain().generate(arguments: arguments, options: options)
         CoordinatorChild().generate(arguments: arguments, options: options)
     }
