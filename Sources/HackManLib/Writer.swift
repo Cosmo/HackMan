329edab5bda0ca7e298763aa90a7e4a34b946c38
Devran "Cosmo" Uenal <maccosmo@gmail.com>
2019-08-15 21:48:21 +0200

    Add helper methods for directory creation based on a file path

--- Sources/HackManLib/Writer.swift
+++ Sources/HackManLib/Writer.swift
@@ -8,19 +8,24 @@
 import Foundation
 
 struct Writer {
-    static func write(contents: String, toFile filePath: String) {
-        var paths = filePath.split(separator: "/")
-        paths.removeLast()
-        
-        _ = paths.reduce("") {(result, next) -> String in
-            if result == "" { return String(next) }
-            
-            let nextPath = result + "/" + next
+    static func basePath(of filePath: String) -> String {
+        var directories = filePath.split(separator: "/")
+        directories.removeLast()
+        return directories.joined(separator: "/")
+    }
+    
+    static func createPath(_ path: String) {
+        var previousPath: String?
+        for path in path.split(separator: "/") {
+            let nextPath = [previousPath, String(path)].compactMap { $0 }.joined(separator: "/")
+            print(nextPath)
             try? FileManager().createDirectory(atPath: nextPath, withIntermediateDirectories: true, attributes: nil)
-            
-            return nextPath
+            previousPath = nextPath
         }
-        
+    }
+    
+    static func write(contents: String, toFile filePath: String) {
+        createPath(basePath(of: filePath))
         try? contents.write(toFile: filePath, atomically: true, encoding: String.Encoding.utf8)
     }
 }
