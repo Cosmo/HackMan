9aca553da2ff33ab397f08ec08ded21d4a3a5d78
Devran "Cosmo" Uenal <maccosmo@gmail.com>
2019-09-02 01:51:26 +0200

    Update Stencil and GrammaticalNumber

--- Package.swift
+++ Package.swift
@@ -11,9 +11,8 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/stencilproject/Stencil.git", from: "0.13.0"),
-        .package(url: "https://github.com/Cosmo/GrammaticalNumber.git", from: "0.0.1")
+        .package(url: "https://github.com/stencilproject/Stencil.git", .branch("master")),
+        .package(url: "https://github.com/Cosmo/GrammaticalNumber.git", from: "0.0.2"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
